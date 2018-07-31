54464f81e6c198617e8741941d8eeaf0b8e36711
Dhanapal <dhanapalan.periasamy@gmail.com>
2018-07-31 23:47:52 +0530

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
